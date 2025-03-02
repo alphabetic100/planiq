f8980a8bebd261294aa5d959368b9a9fc71fd111
alphabetic100 <mh3214002@gmail.com>
2025-03-02 11:06:02 +0600

    Project structure created

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+import 'package:planiq/features/authentication/presentation/screens/login_screen.dart';
+import 'package:planiq/features/authentication/presentation/screens/sing_up_screen.dart';
+import 'package:planiq/features/splash_screen/presentation/screens/splash_screen.dart';
+
+class AppRoute {
+  static String loginScreen = "/loginScreen";
+  static String signUpScreen = "/signUpScreen";
+  static String init = "/";
+
+  static List<GetPage> routes = [
+    GetPage(name: loginScreen, page: () => const LoginScreen()),
+    GetPage(name: init, page: () => const SplashScreen()),
+    GetPage(name: signUpScreen, page: () => const SignUpScreen())
+  ];
+}
