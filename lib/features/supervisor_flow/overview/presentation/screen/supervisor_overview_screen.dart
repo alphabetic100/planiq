bcb0dbf7dbbbe09454bc17ae97eb90c9fc26f509
alphabetic100 <mh3214002@gmail.com>
2025-03-10 11:49:55 +0600

    supervisor overview screen added

--- lib/features/supervisor_flow/overview/presentation/screen/supervisor_overview_screen.dart
+++ lib/features/supervisor_flow/overview/presentation/screen/supervisor_overview_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:planiq/core/common/widgets/app_spacer.dart';
+import 'package:planiq/core/common/widgets/body_padding.dart';
+import 'package:planiq/core/utils/constants/app_sizer.dart';
+import 'package:planiq/features/super_admin_flow/overview/presentation/widget/admin_overview_profile.dart';
+import 'package:planiq/features/supervisor_flow/overview/presentation/widget/supervisor_overview_body.dart';
+import 'package:planiq/features/supervisor_flow/overview/presentation/widget/supervisor_overview_drawer.dart';
 
 class SupervisorOverviewScreen extends StatelessWidget {
-  const SupervisorOverviewScreen({super.key});
+  SupervisorOverviewScreen({super.key});
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Supervisor Overview Screen"),
+      key: _scaffoldKey,
+      drawer: SupervisorOverviewDrawer(),
+      body: Column(
+        children: [
+          AdminOverviewProfile(
+            scaffoldKey: _scaffoldKey,
+          ),
+          VerticalSpace(height: 24.h),
+          BodyPadding(child: SupervisorOverviewBody())
+        ],
       ),
     );
   }
