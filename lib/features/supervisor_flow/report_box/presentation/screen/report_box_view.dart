9f31db0e88d13f713147502f182210155525c90d
alphabetic100 <mh3214002@gmail.com>
2025-03-10 15:28:36 +0600

    feat: implement ReportBoxView and integrate navigation in SupervisorOverviewDrawer

---
+++ lib/features/supervisor_flow/report_box/presentation/screen/report_box_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReportBoxView extends StatelessWidget {
+  const ReportBoxView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
