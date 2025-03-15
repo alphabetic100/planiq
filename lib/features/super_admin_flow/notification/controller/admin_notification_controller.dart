4cdc770494afcc664bec47e34443011293bd692a
alphabetic100 <mh3214002@gmail.com>
2025-03-15 14:17:43 +0600

    Add filtering functionality to admin notifications: implement status and date filters

--- lib/features/super_admin_flow/notification/controller/admin_notification_controller.dart
+++ lib/features/super_admin_flow/notification/controller/admin_notification_controller.dart
@@ -8,11 +8,52 @@ import 'package:planiq/features/super_admin_flow/notification/model/admin_notifi
 
 class AdminNotificationController extends GetxController {
   final NetworkCaller networkCaller = NetworkCaller();
+  RxString statusFilter = "All".obs;
+  RxString dateFilter = "Today".obs;
+  RxString customDate = "".obs;
+  void updateStatusFilter(String value) {
+    statusFilter.value = value;
+  }
+
+  void updateDateFilter(String value) {
+    dateFilter.value = value;
+  }
+
   Rx<AdminNotificationModel?> notifications = Rx<AdminNotificationModel?>(null);
   Future<void> getAdminNotification() async {
     try {
       final response = await networkCaller.getRequest(AppUrls.notification,
           token: AuthService.token);
+      if (response.isSuccess) {
+        log(response.responseData.toString());
+        notifications.value =
+            AdminNotificationModel.fromJson(response.responseData);
+      }
+    } catch (e) {
+      log("Something went wrong, error: $e");
+    }
+  }
+
+  Future<void> filterNotification(String filter) async {
+    try {
+      final response = await networkCaller.getRequest(AppUrls.notification,
+          token: AuthService.token);
+      if (response.isSuccess) {
+        log(response.responseData.toString());
+        notifications.value =
+            AdminNotificationModel.fromJson(response.responseData);
+      }
+    } catch (e) {
+      log("Something went wrong, error: $e");
+    }
+  }
+
+  Future<void> customFilter(String date) async {
+    try {
+      final requestUrl =
+          "${AppUrls.adminNotification}?dateFilter=custom&date=$date";
+      final response =
+          await networkCaller.getRequest(requestUrl, token: AuthService.token);
       if (response.isSuccess) {
         log(response.responseData.toString());
         notifications.value =
